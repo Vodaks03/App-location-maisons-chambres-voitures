525c2e5d18ce6d89481f9c7961805fa5c6e0a323
Vodaks03 <dilanevoumia9@gmail.com>
2025-08-12 22:01:26 +0100

    base

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,9 @@ import 'pages/Discussion.dart';
 import 'pages/Profil.dart';
 
 void main() {
-  runApp(const MaterialApp(home: HomePage()));
+  runApp(
+    const MaterialApp(home: HomePage(), debugShowCheckedModeBanner: false),
+  );
 }
 
 class HomePage extends StatefulWidget {
@@ -33,9 +35,96 @@ class HomePageState extends State<HomePage> {
           pages[pageIndex],
         ],
       ),
+
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 207, 140, 219),
+              ),
+              child: Text(
+                'Menu',
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Acceuil'),
+              onTap: () {
+                setState(() {
+                  pageIndex = 0;
+                });
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.list),
+              title: Text('Liste'),
+              onTap: () {
+                setState(() {
+                  pageIndex = 1;
+                });
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text('Discussion'),
+              onTap: () {
+                setState(() {
+                  pageIndex = 2;
+                });
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Profil'),
+              onTap: () {
+                setState(() {
+                  pageIndex = 3;
+                });
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Paramètres'),
+              onTap: () {
+                // Action pour les paramètres
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text('Déconnexion'),
+              onTap: () {
+                // Action pour la déconnexion
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
-        title: Text("Bienvenue dans Makam"),
+        title: Text("MaKam"),
         centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              // Action pour le bouton de recherche
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: () {
+              // Action pour le bouton plus
+            },
+          ),
+        ],
         backgroundColor: const Color.fromARGB(255, 207, 140, 219),
       ),
 
